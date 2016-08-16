a99b2fa3d1f1b1603699fc3e068b87a19ebe3a2e
Phan Thai Phong <fongfan999@gmail.com>
2016-08-16 16:46:23 +0700

    Localize the currency and translate PAYMENT_TYPES

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,9 @@ module ApplicationHelper
     attributes["style"] = "display: none" if condition
     content_tag("div", attributes, &block)
   end
+
+  def currency_to_locale(price)
+    price *= 1.3 if I18n.locale.to_s == "es"
+    number_to_currency price
+  end
 end
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,2 +1,9 @@
 module OrdersHelper
+  def payment_types_options
+    options = Order::PAYMENT_TYPES.map do |type|
+      [I18n.t("orders.payment_types." + type.parameterize.underscore), type]
+    end
+
+    options_for_select(Hash[options])
+  end
 end
